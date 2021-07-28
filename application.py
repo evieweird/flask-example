83455b8a26125d706ff23ef3f47c51035e9d16f7
Veronica Meyer <eveweird@eve-mac.local>
2021-07-28 15:42:35 +1000

    add

--- application.py
+++ application.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 
 application = Flask(__name__, template_folder='template')
 
+
+
 @application.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', 'database.html')
